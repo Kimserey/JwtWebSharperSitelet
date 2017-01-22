4e9d8d69f103b8ed0dd32a4f0e6cb89c04cdd835
Kimserey Lam <kimserey.lam@groomgy.com>
2017-01-22 23:39:27 +0000

    completed log in

--- Website/Pages.Home.fs
+++ Website/Pages.Home.fs
@@ -16,16 +16,20 @@ module Home =
 
         let page (navigator: PageNavigator) =
             
+            let message =
+                Var.Create ""
+
             match TokenStorage.get() with
-            | Some token -> ()
+            | Some payload ->
+                message.Value <- payload.principal.Identity.Name
             | None -> 
                 TokenStorage.clear()
                 navigator.GoAuth()
 
-            divAttr 
-                [ attr.``class`` "container my-3" ] 
+            divAttr
+                [ attr.``class`` "container my-3" ]
                 [ h1Attr [ attr.``class`` "display-3" ]  [ text "Welcome!" ]
-                  pAttr [ attr.``class`` "lead" ] [ text "You successfuly logged in" ]
+                  pAttr [ attr.``class`` "lead" ] [ message.View |> Doc.BindView (sprintf "You successfuly logged in as %s" >> text) ]
                   hrAttr [ attr.``class`` "my-4" ] []
                   pAttr 
                     [ attr.``class`` "lead" ]
--- Website/TokenStorage.fs
+++ Website/TokenStorage.fs
@@ -5,6 +5,55 @@ open WebSharper
 open WebSharper.JavaScript
 
 [<JavaScript>]
+module JwtHelper =
+
+    type JwtUserIdentity = {
+        Name: string
+        IsAuthenticated: bool
+        AuthenticationType: string
+    } 
+
+    type JwtUserPrincipal = {
+        Identity: JwtUserIdentity
+        Claims: string list
+    }
+
+    type JwtPayload = {
+        principal : JwtUserPrincipal
+        iss: string
+        sub: string
+        iat: int
+        exp : int
+        jti : string
+    } with
+        static member GetTimeSpent stamp x =
+            Math.Round ((DateTime.UtcNow - new DateTime (1970, 1, 1)).TotalSeconds) - stamp
+        
+        static member GetTimeRemaining stamp x = 
+            x.exp - x.iat - JwtPayload.GetTimeSpent stamp x
+    
+    [<Direct """
+        var decodeBase64 = function (s) {
+            var e = {}, i, b = 0, c, x, l = 0, a, r = '', w = String.fromCharCode, L = s.length;
+            var A = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+            for (i = 0; i < 64; i++) { e[A.charAt(i)] = i; }
+            for (x = 0; x < L; x++) {
+                c = e[s.charAt(x)]; b = (b << 6) + c; l += 6;
+                while (l >= 8) { ((a = (b >>> (l -= 8)) & 0xff) || (x < (L - 2))) && (r += w(a)); }
+            }
+            return r;
+        };
+
+        var segments = $token.split('.');
+        if (segments.length !== 3) throw new Error('Not enough or too many segments');
+        var payloadSeg = segments[1];
+        var decodedPayloadSeg = decodeBase64(payloadSeg);
+
+        return JSON.parse(decodedPayloadSeg);
+    """>]
+    let getPayload token = X<JwtPayload>
+
+[<JavaScript>]
 module TokenStorage =
     
     let private authTokenKey = "access_token"
@@ -13,7 +62,7 @@ module TokenStorage =
     let get() = 
         match storage.GetItem authTokenKey with
         | null -> None
-        | token -> Some <| token
+        | token -> Some <| JwtHelper.getPayload token
 
     let set (token: string) = 
         storage.SetItem(authTokenKey, token)
